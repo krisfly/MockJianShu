774fa2dc68af69675faa80b9162075188093547c
fangyu <fangyu@fun.tv>
2016-04-18 14:21:09 +0800

    init projects

---
+++ app/src/main/java/example/kris/mockjianshu/fragment/HomeFragment.java
@@ -0,0 +1,62 @@
+package example.kris.mockjianshu.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.viewpagerindicator.TabPageIndicator;
+
+import example.kris.mockjianshu.R;
+
+/**
+ * Created by kris on 2016/4/3.
+ */
+public class HomeFragment extends Fragment {
+
+    private static final String[] CONTENT = new String[] { "Recent", "Artists", "Albums", "Songs", "Playlists", "Genres","Recent", "Artists", "Albums", "Songs", "Playlists", "Genres" };
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, null);
+
+        FragmentPagerAdapter adapter = new GoogleMusicAdapter(getChildFragmentManager());
+
+        ViewPager pager = (ViewPager)view.findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        TabPageIndicator indicator = (TabPageIndicator)view.findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
+
+        return view;
+    }
+
+    class GoogleMusicAdapter extends FragmentPagerAdapter {
+        public GoogleMusicAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return CONTENT[position % CONTENT.length].toUpperCase();
+        }
+
+        @Override
+        public int getCount() {
+            return CONTENT.length;
+        }
+    }
+}
